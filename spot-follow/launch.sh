7022e14bc77a8c2b0ceb924869d4c9b0d7ed0044
Connor Johnson <35439715+TheGreatChaosCoder@users.noreply.github.com>
2022-11-04 10:59:05 -0500

    adding launch.sh

---
+++ spot-follow/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 ./main.py --username ${SPOT_ACCOUNT} --password ${SPOT_PASSWORD} ${SPOT_IP}
+# For commands that have to be run always at the start of the execution
+
+# Absolute File Paths (from source directory of your OS) /usr/home/app
+# Relative File Paths (from the relative directory the account is at) ./app ./folder
