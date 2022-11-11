9b296d226e8feecba85ac347bf9266aea8c07fb4
jermb <77866654+jermb@users.noreply.github.com>
2022-11-11 10:48:21 -0600

    Startup functions encapsulated

---
+++ spot-follow/CreateSpotMain.py
@@ -0,0 +1,17 @@
+import sys
+import CreateSpot
+
+def main(argv):
+    Spot = CreateSpot.Robot(argv)
+    try:
+        with Spot.keepLeaseAlive():
+            Spot.wake()
+            Spot.stand(0.1)
+            Spot.stand(0.005)
+            Spot.sleep()
+    finally:
+        Spot.getLease()
+
+if __name__ == '__main__':
+    if not main(sys.argv[1:]):
+        sys.exit(1)
