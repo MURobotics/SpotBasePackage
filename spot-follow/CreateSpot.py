4bd4d96a5f84ca283fe519c0c2513b780c280377
Connor Johnson <35439715+TheGreatChaosCoder@users.noreply.github.com>
2022-11-18 10:12:05 -0600

    added all moment functions to CreateSpot

--- spot-follow/CreateSpot.py
+++ spot-follow/CreateSpot.py
@@ -8,8 +8,24 @@ import bosdyn.client.lease
 import bosdyn.client.util
 import bosdyn.geometry
 
+import logging
+
+from bosdyn.client import create_standard_sdk, \
+    RpcError, \
+    lease, \
+    estop, \
+    robot_command, \
+    robot_state, \
+    power, \
+    image, \
+    InvalidRequestError
+
+from bosdyn.client.robot_command import NoTimeSyncError, NotPoweredOnError, RobotCommandBuilder, RobotCommandClient, blocking_stand
+
 from bosdyn.client.image import ImageClient
 from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand
+
+from SpotUtil import Direction, RotationDirection, get_command_duration, pause
 
 #   [docker-compose build] while connected to wifi to compile
 #   [docker-compose up] while connected to spot to run
@@ -88,9 +104,75 @@ class Robot:
 
     #   -----   Movement Commands   -----
 
+    #This functions runs all commands
+    def execute_command(self, cmd, time=None):
+        print("Running Command")
+        try:
+            print(cmd)
+            self.robot.getCommandClient().robot_command(cmd, time)
+        except RpcError:
+            logging.error("Problem communicating with the Spot")
+        except InvalidRequestError:
+            logging.error("Invalid request")
+        except NoTimeSyncError:
+            logging.error("It's been too long since last time-sync")
+        except NotPoweredOnError:
+            logging.error("Engines are not powered")
+        
+        pause(3)
+
     # stand() causes the robot to stand at a given height
     # height is in meters
-    def stand(self, height, delay=3):
-        cmd = RobotCommandBuilder.synchro_stand_command(body_height=height)
-        self.command_client.robot_command(cmd)
-        time.sleep(delay)
+    def stand(self, height):
+        self.execute_command(
+            RobotCommandBuilder.synchro_stand_command(body_height=height)
+        )
+    
+    #Runs a synchro velocity command
+    def move_velocity(self, v_x=0.0, v_y=0.0, v_rot=0.0, duration=0.0):
+        print(f"velocity {v_y}")
+        self.execute_command(
+            RobotCommandBuilder.synchro_velocity_command(
+                v_x=v_x,
+                v_y=v_y,
+                v_rot=v_rot
+            ),
+            get_command_duration(duration)
+        )
+    
+    #Runs a synchro trajectory command
+    #heading in radians, postion relative to robot frame
+    def move_position(self, x_pos=0.0, y_pos=0.0, heading=0.0, duration=0.0):
+        self.execute_command(
+            RobotCommandBuilder.synchro_trajectory_command_in_body_frame(
+                goal_x_rt_body = x_pos, 
+                goal_y_rt_body = y_pos, 
+                goal_heading_rt_body = heading
+            ),
+            get_command_duration(duration)
+        )
+
+    #Moves left or right
+    def strafe(self, speed: float, direction: Direction, duration=0.0):
+        if (not (direction is Direction.LEFT or direction is Direction.RIGHT)):
+            print("No strafing?")
+            raise Exception("Invalid direction for strafing")
+
+        speed = speed if direction is Direction.LEFT else -speed
+        self.move_velocity(0, speed, 0, duration)
+
+    #Moves forwards or backwards
+    def walk(self, speed: float, direction: Direction, duration=0.0):
+        if (not (direction is Direction.FORWARDS or direction is Direction.BACKWARDS)):
+            print("No walking?")
+            raise Exception("Invalid direction for walking")
+
+        speed = speed if direction is Direction.FORWARDS else -speed
+        self.move_velocity(speed, 0, 0, duration)
+
+    #Rotates counterclockwise or clockwise
+    def rotate(self, rot_speed: float, rot_direction: RotationDirection, duration = 0.0):
+        rot_speed = rot_speed if rot_direction is RotationDirection.COUNTERCLOCKWISE else -rot_speed
+        self.move_velocity(0, 0, rot_speed, duration)
+
+    
